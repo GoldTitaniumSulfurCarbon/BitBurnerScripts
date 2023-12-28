e7c40761fde8acf0055997a533cfe1fae8073a20
GoldTitaniumSulfurCarbon <95988349+GoldTitaniumSulfurCarbon@users.noreply.github.com>
2023-12-28 14:42:15 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/** @param {NS} ns */
+//Creates a 16 terabyte server with the name passed as an argument.
 export async function main(ns) {
   const name = ns.args[0]
 const ram = 16384; //Must be some power of 2^x. 16384 seems to be the cap. 55000*RAM for price.
